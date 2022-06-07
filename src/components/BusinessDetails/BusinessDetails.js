7670a6a8a93777f3fe4f497756641a2e7527cadf
Ety-Biswangri <etybgc02@gmail.com>
2022-06-07 16:52:03 +0600

    business detail api is done

--- src/components/BusinessDetails/BusinessDetails.js
+++ src/components/BusinessDetails/BusinessDetails.js
@@ -1,45 +1,129 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../firebase.init';
 
 const BusinessDetails = () => {
+    const [isReload, setIsReload] = useState(false);
+    const [myBusiness, setMyBusiness] = useState({});
+    const [user, loading] = useAuthState(auth);
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        // console.log(data);
+
+        const business = {
+            email: user?.email,
+            businessName: data.businessName,
+            gst: data.gst,
+            address: data.address,
+            businessEmail: data.businessEmail
+        }
+
+        const url = `http://localhost:5000/business/${user?.email}`;
+
+        fetch(url, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(business)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                reset();
+                // toast('Profile Updated');
+                setIsReload(!isReload);
+            })
+    }
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/business/${user?.email}`, {
+            method: 'GET',
+        })
+            .then(res => res.json())
+            .then(result => setMyBusiness(result));
+    }, [user?.email, isReload]);
+
     return (
         <div>
             <h2 className='text-black text-xl text-center mb-6 mt-6 underline font-semibold'>Business Details</h2>
 
             <div class="card flex-shrink-0 max-w-xs lg:w-full lg:max-w-2xl mx-auto shadow-md bg-base-100">
                 <div class="card-body w-full">
-                    <div className='lg:grid grid-cols-2'>
+                    <form onSubmit={handleSubmit(onSubmit)} className='lg:grid grid-cols-2'>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">My Email</span>
+                            </label>
+                            <input type="email" value={user?.email} class="input input-bordered h-8 w-64"  {...register("email")} />
+                        </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Business Name</span>
                             </label>
-                            <input type="text" placeholder="Business Name" class="input input-bordered h-8 w-64" />
+                            <input type="text" placeholder="Business Name" class="input input-bordered h-8 w-64" {...register("businessName", {
+                                required: {
+                                    value: true,
+                                    message: 'Business Name is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.businessName?.type === 'required' && <span className="label-text-alt text-red-500">{errors.businessName?.message}</span>}
+                            </label>
                         </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">GST No</span>
                             </label>
-                            <input type="text" placeholder="GST No" class="input input-bordered h-8 w-64" />
+                            <input type="text" placeholder="GST No" class="input input-bordered h-8 w-64" {...register("gst", {
+                                required: {
+                                    value: true,
+                                    message: 'GST No is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.gst?.type === 'required' && <span className="label-text-alt text-red-500">{errors.gst?.message}</span>}
+                            </label>
                         </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Address</span>
                             </label>
-                            <input type="text" placeholder="Address" class="input input-bordered h-8 w-64" />
+                            <input type="text" placeholder="Address" class="input input-bordered h-8 w-64" {...register("address", {
+                                required: {
+                                    value: true,
+                                    message: 'Address is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.address?.type === 'required' && <span className="label-text-alt text-red-500">{errors.address?.message}</span>}
+                            </label>
                         </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Business Email</span>
                             </label>
-                            <input type="email" placeholder="Business Email" class="input input-bordered h-8 w-64" />
+                            <input type="email" placeholder="Business Email" class="input input-bordered h-8 w-64" {...register("businessEmail", {
+                                required: {
+                                    value: true,
+                                    message: 'Business Email is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.businessEmail?.type === 'required' && <span className="label-text-alt text-red-500">{errors.businessEmail?.message}</span>}
+                            </label>
                         </div>
-                    </div>
-                    <div class="form-control mt-6">
-                        <button class="btn btn-primary btn-sm w-64">Update</button>
-                    </div>
+                        <div class="form-control mt-6">
+                            <button class="btn btn-primary btn-sm w-64">Update</button>
+                        </div>
+                    </form>
                 </div>
             </div>
 
@@ -47,10 +131,10 @@ const BusinessDetails = () => {
                 <h2 className='text-black text-xl text-center mb-0 mt-6 underline font-semibold'>My Business Info</h2>
                 <div class="card-body w-full">
                     <div className='flex flex-col gap-4'>
-                        <p>Business Name: </p>
-                        <p>GST No:</p>
-                        <p>Address: </p>
-                        <p>Business Email:</p>
+                        <p>Business Name: {myBusiness.businessName}</p>
+                        <p>GST No: {myBusiness.gst}</p>
+                        <p>Address: {myBusiness.address}</p>
+                        <p>Business Email: {myBusiness.businessEmail}</p>
                     </div>
                 </div>
             </div>
--- src/components/LoanApplication/LoanApplication.js
+++ src/components/LoanApplication/LoanApplication.js
@@ -1,45 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../firebase.init';
 
 const LoanApplication = () => {
+    const [isReload, setIsReload] = useState(false);
+    const [myLoan, setMyLoan] = useState({});
+    const [user, loading] = useAuthState(auth);
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+
     return (
         <div>
             <h2 className='text-black text-xl text-center mb-6 mt-6 underline font-semibold'>Loan Application</h2>
 
             <div class="card flex-shrink-0 max-w-xs lg:w-full lg:max-w-2xl mx-auto shadow-md bg-base-100">
                 <div class="card-body w-full">
-                    <div className='lg:grid grid-cols-2'>
+                    <form onSubmit={handleSubmit(onSubmit)} className='lg:grid grid-cols-2'>
+
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input type="email" value={user?.email} class="input input-bordered h-8 w-64"  {...register("email")} />
+                        </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Loan Amount</span>
                             </label>
-                            <input type="number" placeholder="Loan Amount" class="input input-bordered h-8 w-64" />
+                            <input type="number" placeholder="Loan Amount" class="input input-bordered h-8 w-64" defaultValue="10000" {...register("amount", {
+                                required: {
+                                    value: true,
+                                    message: 'Loan Amount is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.amount?.type === 'required' && <span className="label-text-alt text-red-500">{errors.amount?.message}</span>}
+                            </label>
                         </div>
 
                         <div class="form-control">
                             <label class="label">
-                                <span class="label-text">Interest Rate</span>
+                                <span class="label-text">Interest Rate (%)</span>
                             </label>
-                            <input type="number" placeholder="GST No" class="input input-bordered h-8 w-64" />
+                            <input type="number" placeholder="Interest Rate" class="input input-bordered h-8 w-64" defaultValue="5" {...register("interest", {
+                                required: {
+                                    value: true,
+                                    message: 'Interest Rate is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.interest?.type === 'required' && <span className="label-text-alt text-red-500">{errors.interest?.message}</span>}
+                            </label>
                         </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Loan Tenure (in years)</span>
                             </label>
-                            <input type="number" placeholder="Loan Tenure (in years)" class="input input-bordered h-8 w-64" />
+                            <input type="number" placeholder="Loan Tenure (in years)" class="input input-bordered h-8 w-64" defaultValue="10" {...register("tenure", {
+                                required: {
+                                    value: true,
+                                    message: 'Loan Tenure is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.tenure?.type === 'required' && <span className="label-text-alt text-red-500">{errors.tenure?.message}</span>}
+                            </label>
                         </div>
 
                         <div class="form-control">
                             <label class="label">
                                 <span class="label-text">Minimum Monthly Payment</span>
                             </label>
-                            <input type="number" placeholder="Minimum Monthly Payment" class="input input-bordered h-8 w-64" />
+                            <input type="number" placeholder="Minimum Monthly Payment" class="input input-bordered h-8 w-64" defaultValue="2000" {...register("monthlyPayment", {
+                                required: {
+                                    value: true,
+                                    message: 'Minimum Monthly Payment is required'
+                                },
+                            })} />
+                            <label className="label">
+                                {errors?.monthlyPayment?.type === 'required' && <span className="label-text-alt text-red-500">{errors.monthlyPayment?.message}</span>}
+                            </label>
                         </div>
-                    </div>
-                    <div class="form-control mt-6">
-                        <button class="btn btn-primary btn-sm w-64">Submit</button>
-                    </div>
+
+                        <div class="form-control mt-6">
+                            <button class="btn btn-primary btn-sm w-64">Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
 
